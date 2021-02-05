00ada2355eb5afcb560006d37ef8c14485ef9daa
k-mccalla <kmccalla@qa.com>
2021-02-05 12:26:36 +0000

    order items table and class created

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -2,9 +2,6 @@ package com.qa.ims.persistence.dao;
 
 import java.sql.Connection;
 
-
-
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,31 +10,42 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.persistence.domain.Orders_Items;
 import com.qa.ims.utils.DBUtils;
 
 public class OrderDAO implements Dao<Order> {
 
 	public static final Logger LOGGER = LogManager.getLogger();
-	
 
 //should it be override?
 	public Order modelFromResultSet1(ResultSet resultSet) throws SQLException {
 		Long orderID = resultSet.getLong("order_id");
 		Long price = resultSet.getLong("price");
 		Long quantity = resultSet.getLong("quantity");
-		String date = resultSet.getString("date"); 
+		String date = resultSet.getString("date");
 		Long fkID = resultSet.getLong("fk_id");
-		Long fkItemID = resultSet.getLong("fk_item_id");
-		return new Order(orderID, price, quantity, date,  fkID, fkItemID);
+//		Long saleID = resultSet.getLong("sale_id");
+//		Long fkOrderID = resultSet.getLong("fk_order_id");
+//		Long fkItemID = resultSet.getLong("fk_item_id");
+		return new Order(orderID, price, quantity, date, fkID);
 
 	}
+
+	public Orders_Items modelFromResultSet2(ResultSet resultSet) throws SQLException {
+		Long saleID = resultSet.getLong("sale_id");
+		Long fkOrderID = resultSet.getLong("fk_order_id");
+		Long fkItemID = resultSet.getLong("fk_item_id");
+
+        return new Orders_Items(saleID, fkOrderID, fkItemID);
+
+	}
+	
 
 	@Override
 	public List<Order> readAll() {
@@ -55,8 +63,6 @@ public class OrderDAO implements Dao<Order> {
 		}
 		return new ArrayList<>();
 	}
-
-
 
 	@Override
 	public Order read(Long orderID) {
@@ -78,54 +84,66 @@ public class OrderDAO implements Dao<Order> {
 	@Override
 	public Order create(Order order) {
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection.prepareStatement(
-						"INSERT INTO orders(price, quantity, date, fk_id, fk_item_id) VALUES (?, ?, ?, ?, ?)");) {
-		statement.setLong(1, order.getPrice());
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO orders(price, quantity, date, fk_id) VALUES (?, ?, ?, ?)");) {
+			statement.setLong(1, order.getPrice());
 			statement.setLong(2, order.getQuantity());
 			statement.setString(3, order.getDate());
 			statement.setLong(4, order.getFkID());
-			statement.setLong(5, order.getFkItemID());
+
 			statement.executeUpdate();
-			return readLatest();
+
+			try (Connection connection1 = DBUtils.getInstance().getConnection();
+					PreparedStatement statement1 = connection1.prepareStatement(
+							"INSERT INTO order_items(sales_id, order_id item_id) VALUES (?, ?, ?)");) {
+				statement.setLong(1, Orders_Items.getSaleID());
+				statement.setLong(2, Orders_Items.getFkOrderID());
+				statement.setLong(3, Orders_Items.getFkItemID());
+				statement.executeUpdate();
+				return readLatest();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return order;
+
+	}
+
+	public Order readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY order_id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet1(resultSet);
 		} catch (Exception e) {
 			LOGGER.debug(e);
 			LOGGER.error(e.getMessage());
-		
-		
 		}
 		return null;
 	}
-public Order readLatest() {
-	try (Connection connection = DBUtils.getInstance().getConnection();
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY order_id DESC LIMIT 1");) {
-		resultSet.next();
-		return modelFromResultSet1(resultSet);
-	} catch (Exception e) {
-		LOGGER.debug(e);
-		LOGGER.error(e.getMessage());
-	}
-		return null;
-	}
 
-	//unsure do need update order?
+	// unsure do need update order?
 	@Override
 	public Order update(Order t) {
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				PreparedStatement statement = connection
-						.prepareStatement("UPDATE orders SET price = ?, quantity = ?, date = ?, fk_id = ?, fk_item_id = ? WHERE order_id = ?");) {
-			statement.setLong(1, t.getPrice());	
+				PreparedStatement statement = connection.prepareStatement(
+						"UPDATE orders SET price = ?, quantity = ?, date = ?, fk_id = ? WHERE order_id = ?");) {
+			statement.setLong(1, t.getPrice());
 			statement.setLong(2, t.getQuantity());
-			statement.setString(3, t.getDate());          
+			statement.setString(3, t.getDate());
 			statement.setLong(4, t.getFkID());
-			statement.setLong(5, t.getFkItemID());
+			// statement.setLong(5, t.getFkItemID());
 			statement.executeUpdate();
 			return read(t.getOrderID());
 		} catch (Exception e) {
 			LOGGER.debug(e);
 			LOGGER.error(e.getMessage());
 		}
-
 
 		return null;
 	}
@@ -140,8 +158,7 @@ public Order readLatest() {
 			LOGGER.debug(e);
 			LOGGER.error(e.getMessage());
 		}
-		
-		
+
 		return 0;
 	}
 
@@ -152,4 +169,3 @@ public Order readLatest() {
 	}
 
 }
-	
